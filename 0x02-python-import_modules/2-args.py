9720116247767754469fa7de2bb4718d259c96de
FisaniG <fisanig@gmail.com>
2022-12-01 14:13:13 -0800

    fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     a = len(argv) - 1
     b = 1
     if a < 1:
-        print("{:d} argument.".format(a))
+        print("{:d} arguments.".format(a))
     elif a == 1:
         print("{:d}: argument:".format(a))
-        print("{:d}: {:s}".format(b, argv[b]))
+        print("{:d}: {:s}".format(a, argv[a]))
     else:
         print("{:d} arguments:".format(a))
         while(a > 0):
