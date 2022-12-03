7aff2d2e0bf56f79d10d4f5f55ab0191d468fbb5
FisaniG <fisanig@gmail.com>
2022-12-03 12:14:50 -0800

    tak 0 - 7

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    newstr = ""
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            pass
+        else:
+            newstr += i
+    return newstr
