c70b2ed781766f774a698f0d96b4b1da613c2209
FisaniG <fisanig@gmail.com>
2022-12-08 04:01:36 -0800

    remove space

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     size = (len(argv) - 1)
     if size == 1:
-        print(" 1 argument:")
+        print("{0} argument:".format(size))
     elif size < 1:
-        print("0 arguments.")
+        print("{0} arguments.".format(size))
     else:
-        print("{:d} arguments:".format(size))
+        print("{0} arguments:".format(size))
     for i in range(1, size + 1):
-        print("{:d}:{:s}".format(i, argv[i]))
+        print("{0}: {1}".format(i, argv[i]))
